e3759575267e20039946827fd993efdd419c5c90
pablo014 <angelopablo014@gmail.com>
2022-12-04 16:04:02 -0700

    feat(assets): add images

--- src/components/TBoard.vue
+++ src/components/TBoard.vue
@@ -1,11 +1,10 @@
 <script setup>
+import TItem from './TItem.vue';
 </script>
 <template>
     <div class="flex flex-wrap">
-        <div class="w-1/3 border-2 border-black" v-for="index in 9" :key="index">
-            <div>
-                test
-            </div>
+        <div class="w-1/3 border-2 border-black flex items-center justify-center" v-for="index in 9" :key="index">
+            <TItem :isX="false" />
         </div>
     </div>
 </template>
---
+++ src/components/TItem.vue
@@ -0,0 +1,13 @@
+<script setup>
+import {defineProps} from 'vue';
+const props = defineProps({
+    isX: Boolean,
+});
+</script>
+<template>
+    <div>
+        <img src="@/assets/cross.png" v-if="props.isX" />
+        <img src="@/assets/circle.png" v-else />
+    </div>
+</template>
+<style></style>
