aaa2655a0cff05b8060bcd00f2b79d28cab10cf1
pablo014 <angelopablo014@gmail.com>
2022-12-04 15:44:02 -0700

    feat(table): create table

---
+++ src/components/TBoard.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+<template>
+    <div class="flex flex-wrap">
+        <div class="w-1/3 border-2 border-black" v-for="index in 9" :key="index">
+            <div>
+                test
+            </div>
+        </div>
+    </div>
+</template>
+<style></style>
---
+++ src/components/Tile.vue
@@ -0,0 +1,12 @@
+<script setup>
+const props = defineProps({
+    isX: bool,
+});
+</script>
+<template>
+    <div>
+        <img src="src/assets/cross.png" v-if="isX" />
+        <img src="src/assets/circle.png" v-else />
+    </div>
+</template>
+<style></style>
