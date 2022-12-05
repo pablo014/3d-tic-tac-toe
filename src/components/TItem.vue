197aa0957cb7883a820d8e4d0548ae22f1454953
pablo014 <angelopablo014@gmail.com>
2022-12-05 12:20:42 -0700

    feat(board): at clickability

--- src/components/TItem.vue
+++ src/components/TItem.vue
@@ -1,13 +1,20 @@
 <script setup>
-import {defineProps} from 'vue';
+import {defineProps, ref} from 'vue';
 const props = defineProps({
     isX: Boolean,
 });
+const isSelected = ref(false);
 </script>
 <template>
-    <div>
-        <img src="@/assets/cross.png" v-if="props.isX" />
-        <img src="@/assets/circle.png" v-else />
+    <div class="h-20">
+        <div v-if="isSelected">
+            <img class="h-20" src="@/assets/cross.png" v-if="props.isX" />
+            <img class="h-20" src="@/assets/circle.png" v-else />
+        </div>
+        <div @click="(isSelected = true)" v-else>
+            <img class="h-20 opacity-0 hover:opacity-5" src="@/assets/cross.png" v-if="props.isX" />
+            <img class="h-20 opacity-0 hover:opacity-5" src="@/assets/circle.png" v-else />
+        </div>
     </div>
 </template>
 <style></style>
